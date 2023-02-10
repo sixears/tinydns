428d59d9e8c4c9b2bdc9b23db82dc835265cc4cb
Mr. Fluffy <git@sixearsm>
2023-02-10 06:16:11 +0000

    remove Paths.hs from src/; use the generated version from proto

--- src/TinyDNS/Paths.hs
+++
@@ -1,6 +0,0 @@
-module TinyDNS.Paths where
-
-import FPath.AbsFile  ( AbsFile, absfile )
-
-tinydns_edit :: AbsFile
-tinydns_edit = [absfile|/nix/store/0bcidyqw1r1pi8ibqk0mjwqgv2505c7c-djbdns-1.05/bin/tinydns-edit|]
